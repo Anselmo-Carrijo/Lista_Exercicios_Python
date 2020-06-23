ef0f101502ecb8532414d586d70d4ce82777042c
Anselmo Carrijo Gomes <54807443+anselmocarrijog@users.noreply.github.com>
2020-06-23 09:42:00 -0300

    Lista de exercicios Python

--- EstruturaDeRepeticao/ex48.py
+++ EstruturaDeRepeticao/ex48.py
@@ -11,7 +11,7 @@ reverso = 0
 
 while aux != 0:
     reverso = reverso * 10 + aux % 10
-    aux //= 10
+    aux = aux // 10
 
-print("Reverso de", n, ":")
+print("Reverso de", n, ": ")
 print(reverso)
