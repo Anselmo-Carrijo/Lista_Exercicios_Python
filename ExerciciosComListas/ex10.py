7842f8695864020f7b1e2d310a733da307466513
Anselmo Carrijo Gomes <54807443+anselmocarrijog@users.noreply.github.com>
2020-06-26 11:31:26 -0300

    Lista de exercicios Python

--- ExerciciosComListas/ex10.py
+++ ExerciciosComListas/ex10.py
@@ -1,20 +1,34 @@
 """
-Faça um Programa que leia dois vetores com 10 elementos cada.
-Gere um terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
-
+Faça um Programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos,
+cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 """
-a = []
-b = []
+a = list()
+b = list()
+inter = list()
 
 c = 0
-while c != 3:
-    i = 0
-    n = input('Informe um elemento para lista A:\n')
+while c != 10:
+    n = int(input('Informe o {}° número do Vetor A.\n'.format(c + 1)))
+    print('====' * 10)
     a.append(n)
     c += 1
+
 c = 0
-while c != 3:
-    i = 0
-    n = input('Informe um elemento para lista B:\n')
+while c != 10:
+    n = int(input('Informe o {}° número do Vetor B.\n'.format(c + 1)))
+    print('======' * 10)
     b.append(n)
     c += 1
+
+c = 0
+while c != 10:
+    inter.append(a[c])
+    inter.append(b[c])
+    c += 1
+
+print(f'A primeira lista é {a}')
+print('======='*10)
+print(f'A Segunda  lista é {b}')
+print('========='*10)
+print(f'A lista intercalada é {inter}')
+print('========='*10)
