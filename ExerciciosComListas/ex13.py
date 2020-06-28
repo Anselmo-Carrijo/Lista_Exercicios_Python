2f976e53dbd22e68bc96dcd2952b9aebbd17d91b
Anselmo Carrijo Gomes <54807443+anselmocarrijog@users.noreply.github.com>
2020-06-27 23:53:43 -0300

    Update ex13.py

--- ExerciciosComListas/ex13.py
+++ ExerciciosComListas/ex13.py
@@ -4,3 +4,22 @@ a média anual das temperaturas e mostre todas as temperaturas acima da média a
 (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro, . . . ).
 
 """
+print('Informe o mês que deseja inserir as temperaduras.\n'
+      'Basta escolher as opções para mudar os meses.')
+
+print('1 - Janeiro:\n'
+      '2 - Fevereiro:\n'
+      '3 - Março:\n'
+      '4 - Abril:\n'
+      '5 - Maio:\n'
+      '6 - Junho:\n'
+      '7 - Julho:\n'
+      '8 - Agosto:\n'
+      '9 - Setembro:\n'
+      '10 - Outubro:\n'
+      '11 - Novembro:\n'
+      '12 - Dezembro:\n')
+print('===='*15)
+mes = int(input('Informe o mês que deseja cadastrar as temperaturas:\n'))
+
+
